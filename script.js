ea598ca09a93c41d40987998bf2f27abe8a9da6c
Felipe Dovigo <142946002+FDovigo@users.noreply.github.com>
2024-02-20 12:49:49 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,110 @@
+const containerVideos = document.querySelector(".videos__container");
+
+
+async function buscarEMostrarVideos(){
+    try{
+        const busca = await fetch("http://localhost:3000/videos");
+        const videos = await busca.json();
+
+            videos.forEach((video)=> {
+                if(video.categoria == ""){
+                    throw new Error('Vídeo não tem categoria');
+                }
+                containerVideos.innerHTML += `
+                <li class="videos__item">
+                    <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
+                    <div class="descricao-video">
+                        <img class="img-canal" src="${video.imagem} alt="Logo do Canal">
+                        <h3 class="titulo-video">${video.titulo}</h3>
+                        <p class="titulo-canal">${video.descricao}</p>
+                        <p class="categoria" hidden>${video.categoria}</p>
+                    </div>
+                </li>
+                `;
+            })
+    } catch(error){
+        containerVideos.innerHTML = `<p> Houve um erro ao carregar os vídeos: ${error}</p>`
+    }
+}
+
+
+buscarEMostrarVideos();
+
+
+const barraDePesquisa = document.querySelector(".pesquisar__input");
+
+barraDePesquisa.addEventListener("input", filtrarPesquisa);
+
+// function filtrarPesquisa(){
+//     const videos = document.querySelectorAll(".videos__item");
+
+//     if(barraDePesquisa.value != ""){
+//         for(let video of videos){
+//             let titulo = video.querySelector(".titulo-video").textContent.toLowerCase();
+//             let valorFiltro = barraDePesquisa.value.toLowerCase();
+
+//             if(!titulo.includes(valorFiltro)){
+//                 video.style.display = "none";
+//             } else {
+//                 video.style.display = "block";
+//             }
+
+//         }
+//     } else {
+//         video.style.display = "block";
+//     }
+// }
+
+//a função acima por simplificada da seguinte maneira:
+function filtrarPesquisa() {
+    const videos = document.querySelectorAll('.videos__item');
+    const valorFiltro = barraDePesquisa.value.toLowerCase();
+  
+    videos.forEach((video) => {
+      const titulo = video.querySelector('.titulo-video').textContent.toLowerCase();
+
+      //essa função é como o "ses" no Excel
+      //no caso, o primeiro IF é o "valorFiltro?" true = titulo.includes(valorFiltro), false = 'block' (o último block)
+      //o segundo IF é o "titulo.includes(valorFiltro)?" true = 'block', false = 'none'
+      //pode escrever assim:
+      //if(valorFiltro) {
+      //    if(titulo.includes(valorFiltro)) {
+      //        video.style.display = 'block' 
+      //    } else {
+      //        video.style.display = 'none'
+      //    }
+      //} else {
+      //    video.style.display = 'block' 
+      //}
+      video.style.display = valorFiltro ? titulo.includes(valorFiltro) ? 'block' : 'none' : 'block';
+    });
+}
+
+// Substituição do Loop for...of por forEach: O loop for...of é substituído por 
+// forEach para percorrer os elementos videos. Isso torna o código mais legível e conciso.
+
+// Condição Ternária: A lógica condicional que define o estilo de exibição (display) 
+// dos vídeos é simplificada usando uma condição ternária. Isso elimina a necessidade 
+// de blocos if...else.
+
+
+const botaoCategoria = document.querySelectorAll(".superior__item");
+
+botaoCategoria.forEach((botao) => {
+    let nomeCategoria = botao.getAttribute("name");
+    botao.addEventListener("click", () => filtrarPorCategoria(nomeCategoria));
+})
+
+function filtrarPorCategoria(filtro){
+    const videos = document.querySelectorAll(".videos__item");
+    for(let video of videos){
+        let categoria = video.querySelector(".categoria").textContent.toLowerCase();
+        let valorFiltro = filtro.toLowerCase();
+
+        if(!categoria.includes(valorFiltro) && valorFiltro != 'tudo'){
+            video.style.display = "none";
+        } else {
+            video.style.display = "block";
+        }
+    }
+}
